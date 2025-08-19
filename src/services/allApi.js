6a52c13e83202757a0bd8e84629ae593249b2a1b
jithin pm <jithinpm.official@gmail>
2025-08-19 11:47:02 +0530

    Recent changes and text updation axxording to client requirements

--- src/services/allApi.js
+++ src/services/allApi.js
@@ -1,7 +1,6 @@
 import { BASE_URL } from "./baseURL";
 import { commonApi } from "./CommonApi";
 
-
 export const sendContactMessageApi = async (reqBody) => {
     return await commonApi("post", `${BASE_URL}/contact_us`, reqBody, "")
 }
